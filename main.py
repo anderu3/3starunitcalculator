085a48ba077e1920aa72f58eb5cfaf0c1a5edd3a
ANDREW LEE <qaswerty1@gmail.com>
2024-04-23 18:04:14 -0700

    Initialized variables for probability equation

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import random
+import odds
 
 SHOP_SLOTS = 5
 
-game = Gamestate(current_comp="", champ_gone=[], other_champs=[], level=0,percent_of_hitting=0)
+target_unit_cost = int(input("What cost unit is this?: ")) # tar_unit_cost = 3
 
-#Input how much of that specific 3 cost you have
 currently_owned_unit = int(input("How many copies of this unit do you have?: "))
+odds.champion_bag_pool['3-cost'] = odds.champion_bag_pool['3-cost'] - currently_owned_unit
 
-#Input how much of that specific 3 cost others have
 others_owned_unit = int(input("How many copies do other players have of this unit?: "))
 
-#Input how much 3 costs are out there thats not that unit
 other_units_that_cost = int(input("How many 3 star copies are taken?: "))
-#Input your level
 
 user_level = int(input("What is your level?: "))
 #Input gold (extra functionality)
 #Return percent of hitting 3 star at your level
 
-def chance_to_roll():
+iteration = 2
+total_pool = 
+for i in range(iteration):
+
 
 #Return percent of hitting at that gold (extra)
 
-
-one_roll = shop_odds["Level 7"]["3-cost"] ** SHOP_SLOTS
-print(one_roll)
 
 # Each shop has
